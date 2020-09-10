1f4ed988027276a3bcbac5338d127626ef743646
jeethpawar <jeeth.pawar@gmail.com>
2020-09-09 19:50:55 -0700

    webpage redesign, full form implementation aded

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 from django.forms import MultipleChoiceField
 from django.forms import CheckboxSelectMultiple
+import pandas as pd
+
+def load_options():
+    df2 = pd.read_csv('data/symptom_ids.csv')
+    res = []
+
+    for i in range(len(df2.columns)):
+        if i > 0:
+            key = str(i)
+            symptom_id = str(i-1)
+            res.append([symptom_id, df2[key][0]])
+    return tuple(res)
 
 
 class SymptomForm(forms.Form):
-    OPTIONS = (
-        ['0', "Cough"],
-        ['1', 'Bodyache'],
-        ['2', 'Fever'],
-    )
+    OPTIONS = load_options()
     Symptoms = forms.MultipleChoiceField(widget = forms.CheckboxSelectMultiple, choices = OPTIONS)
 
     def as_model_input(self):
@@ -17,3 +25,5 @@ class SymptomForm(forms.Form):
         for symptom_id in symptoms:
             results[int(symptom_id)] = True
         return results
+
+

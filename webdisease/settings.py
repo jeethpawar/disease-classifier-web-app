8c226a601908a653f3d8324e37ae0ad61c9cdf93
jeethpawar <jeeth.pawar@gmail.com>
2020-09-27 15:13:35 -0700

    changed settings for heroku deployment

--- webdisease/settings.py
+++ webdisease/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/catalog/static/'
 SITE_ID = 1
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'catalog/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

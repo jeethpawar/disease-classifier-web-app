1ac1fb9ead07761f69c88cf18119bbf14bd7ac0b
jeethpawar <jeeth.pawar@gmail.com>
2020-09-29 17:40:44 -0700

    testing top100 model

--- catalog/views.py
+++ catalog/views.py
@@ -16,7 +16,7 @@ from joblib import dump, load
 import pandas as pd
 import logging
 
-clf = load('model/top10model.joblib')
+clf = load('model/top100model.joblib')
 #clf = load('model/fullmodel.joblib')
 diseaseInfo = DiseaseInfo()
 autocomplete = AutocompleteSystem()
